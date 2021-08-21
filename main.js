05b02269cf7d95f1e5da6a095a979002e4148457
theikoniksir <76625520+theikoniksir@users.noreply.github.com>
2021-08-21 22:20:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+canvas=
+document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle =  "red";
+ctx.lineWidth  = 1;
+ctx.rect(150,143, 430,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle =  "red";
+ctx.lineWidth  = 1;
+ctx.arc(250,210, 40,0, 2 * math.PI);
+
+ctx.stroke();ctx.beginPath();
+ctx.strokeStyle =  "red";
+ctx.lineWidth  = 1;
+ctx.arc(250,210, 40,0, 2 * math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle =  "red";
+ctx.lineWidth  = 1;
+ctx.arc(250,210, 40,0, 2 * math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle =  "red";
+ctx.lineWidth  = 1;
+ctx.arc(250,210, 40,0, 2 * math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle =  "red";
+ctx.lineWidth  = 1;
+ctx.arc(250,210, 40,0, 2 * math.PI);
+ctx.stroke();
